c20f44b8a5acaab1c63a3534e37e5b8c2da015c8
Agustin Nunez <anunez@gmail.com>
2015-12-18 08:52:36 +0000

    Update CustomChars.ino

--- examples/CustomChars/CustomChars.ino
+++ examples/CustomChars/CustomChars.ino
@@ -1,3 +1,11 @@
+  // ESP8266 with 16x2 i2c LCD
+  // Compatible with the Arduino IDE 1.6.6
+  // Library https://github.com/agnunez/ESP8266-I2C-LCD1602
+  // Original Library https://github.com/fdebrabander/Arduino-LiquidCrystal-I2C-library
+  // Modified for ESP8266 with GPIO0-SDA GPIO2-SCL and LCD1206 display
+  // edit library and change Wire.begin() by Wire.begin(sda,scl) or other GPIO's used for I2C
+  // and access from lcd.begin(sda,scl)
+  
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
